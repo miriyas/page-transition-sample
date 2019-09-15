7425679389ddbb0b121c28b2c77e04b29f1009fe
miriya <miriya.lee@gmail.com>
2019-09-15 21:19:11 +0900

    기본적인 탭 내비게이션 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Router, Link } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
+
 import './App.css';
+import image1 from './image1.jpg';
+import image2 from './image2.jpg';
+import image3 from './image3.jpg';
+import image4 from './image4.jpg';
+import image5 from './image5.jpg';
+
+const Page1 = () => <div className="page" style={{ backgroundImage: `url(${image1})`}} />;
+const Page2 = () => <div className="page" style={{ backgroundImage: `url(${image2})`}} />;
+const Page3 = () => <div className="page" style={{ backgroundImage: `url(${image3})`}} />;
+const Page4 = () => <div className="page" style={{ backgroundImage: `url(${image4})`}} />;
+const Page5 = () => <div className="page" style={{ backgroundImage: `url(${image5})`}} />;
+const Footer = () => (
+  <nav className="footer">
+    <Link to='/'>쯔위</Link>
+    <Link to='/2'>손나은</Link>
+    <Link to='/3'>수지</Link>
+    <Link to='/4'>아이유</Link>
+    <Link to='/5'>설현</Link>
+  </nav>
+)
+
+const history = createBrowserHistory()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Page1} />
+        <Route exact path="/2" component={Page2} />
+        <Route exact path="/3" component={Page3} />
+        <Route exact path="/4" component={Page4} />
+        <Route exact path="/5" component={Page5} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
