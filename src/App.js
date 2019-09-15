730089d2027e1cf4026e6e6fd6a54ed71182124c
miriya <miriya.lee@gmail.com>
2019-09-15 21:21:09 +0900

    간단한 디졸브 애니메이션 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, Route, Router, Link } from 'react-router-dom'
 import { createBrowserHistory } from 'history';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
-import './App.css';
+import './App.scss';
 import image1 from './image1.jpg';
 import image2 from './image2.jpg';
 import image3 from './image3.jpg';
@@ -14,6 +15,7 @@ const Page2 = () => <div className="page" style={{ backgroundImage: `url(${image
 const Page3 = () => <div className="page" style={{ backgroundImage: `url(${image3})`}} />;
 const Page4 = () => <div className="page" style={{ backgroundImage: `url(${image4})`}} />;
 const Page5 = () => <div className="page" style={{ backgroundImage: `url(${image5})`}} />;
+
 const Footer = () => (
   <nav className="footer">
     <Link to='/'>쯔위</Link>
@@ -26,16 +28,37 @@ const Footer = () => (
 
 const history = createBrowserHistory()
 
+const pageTrans = 'trans'
+const classNames = {
+  appear: `${pageTrans} appear`,
+  appearActive: `${pageTrans} appear active`,
+  appearDone: `${pageTrans} appear done`,
+  enter: `${pageTrans} enter`,
+  enterActive: `${pageTrans} enter active`,
+  enterDone: `${pageTrans} enter done`,
+  exit: `${pageTrans} exit`,
+  exitActive: `${pageTrans} exit active`,
+  exitDone: `${pageTrans} exit done`
+}
+
 function App() {
   return (
     <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={Page1} />
-        <Route exact path="/2" component={Page2} />
-        <Route exact path="/3" component={Page3} />
-        <Route exact path="/4" component={Page4} />
-        <Route exact path="/5" component={Page5} />
-      </Switch>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup className='transitionGroup'>
+            <CSSTransition key={location.pathname} classNames={classNames} timeout={200}>
+              <Switch location={location}>
+                <Route exact path="/" component={Page1} />
+                <Route exact path="/2" component={Page2} />
+                <Route exact path="/3" component={Page3} />
+                <Route exact path="/4" component={Page4} />
+                <Route exact path="/5" component={Page5} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
       <Footer />
     </Router>
   );
